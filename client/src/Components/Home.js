21eae5b567b8911887dd773008d809110cc32e60
Ali Al Shaikhly <alshaikhly89@gmail.com>
2020-09-22 19:19:09 -0600

    fix size of navbar

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Nav from './Nav'
+import './Home.css'
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      <h1>Hello World</h1>
+      <main className='home'>
+        <h1>Hello World</h1>
+      </main>
     </div>
   )
 }
